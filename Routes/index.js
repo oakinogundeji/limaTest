e37bd8d2a96fef44f2c3e76d3812725f8812f67d
oakinogundeji <oakinogundeji@gmail.com>
2023-02-22 14:28:40 +0100

    refactor complete

--- Routes/index.js
+++ Routes/index.js
@@ -5,36 +5,26 @@
 const
     express = require('express'),
     Router = express.Router(),
-    DataObjects = require('../models/dataObjects');
+    Samples = require('../models/sample');
 
 /**
  * Route to save Data
  */
-Router.post('/saveData', async (req, res) => {
-    const data = req.body;
-    if(!data.id) {
-        return res.status(404).json({
-            msg: 'there was an error saving the data, please ensure that the "id" attribute is present. Thank you'
-        });
-
-    }
-    const dataProps = Object.keys(data);
-    const NewDataObjectDoc = new DataObjects({});
-    dataProps.forEach(val => {
-        NewDataObjectDoc[val] = data[val];
+Router.post('/submit', async (req, res) => {
+    const data = req.body.data;
+    const NewSampleDoc = new Samples({
+        data
     });
-
     try {
-        await NewDataObjectDoc.save();
+        await NewSampleDoc.save();
         return res.status(200).json({
             msg: 'save success',
-            data: NewDataObjectDoc.id
+            data: NewSampleDoc.data
         });
     }
     catch (err) {
-        console.log('err saving data', err);
-        return res.status(404).json({
-            msg: 'there was an error saving the data, please ensure all field values are of the correct data type, or ensure that the "id" attribute is unique. Thank you',
+        return res.status(500).json({
+            msg: 'there was an error saving the data',
             data: err
         });
     }
@@ -44,24 +34,16 @@ Router.post('/saveData', async (req, res) => {
  * Route to retrieve Data
  */
 Router.get('/getData', async (req, res) => {
-    const objID = req.query.id;
     try {
-        const dataObj = await DataObjects.findOne({id: objID}, {_id: 0, __v: 0});
-        if(!dataObj) {
-            return res.status(404).json({
-                msg: `data with id: ${objID} does NOT exist, please confirm the correct ID. Thank you.`,
-                data: dataObj
-            })
-
-        }
+        const dataObj = await Samples.findOne({});
         return res.status(200).json({
             msg: 'retrieve success',
-            data: dataObj
+            data: dataObj.data
         });
     }
     catch (err) {
         return res.status(500).json({
-            msg: 'there was an internal server error retrieving the data, please try again later or contact the service provider. THank you',
+            msg: 'there was an error retrieveing the data',
             data: err
         });
     }
